ce17432b4a68efc8817bbd7b0557576370421fea
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-08-24 17:08:46 +0200

    new: Defined p4 for Elixir.

---
+++ src/exs/p4.exs
@@ -0,0 +1,58 @@
+defmodule Functionz do
+
+  # Only Elixir allows definition of pattern matched functions like these:
+  # NOTE: order of defintion matters! (cause order of pattern matching)
+  def fun1 "abc" do
+    "abc"
+  end
+
+  def fun1 other do
+    other
+  end
+
+end
+
+
+defmodule Main do
+  import Functionz
+
+
+  def main do
+
+    # "regular" functions:
+    # NOTE: operator notation:
+    IO.puts fun1 "abc"
+    IO.puts fun1 "other"
+
+    # anonymous functions:
+    fun3 = fn argument when is_number(argument) -> argument + 1 end
+    fun_in_fun = fn -> fun3.(1) + 1 end
+
+    # lazy evaluation
+    IO.puts inspect {fun3, fun_in_fun} # lazy, no invocation, tuple of functions
+    # eager evaluation
+    IO.puts inspect fun_in_fun.() # eager - invoke function
+    IO.puts inspect fun3.(5) # eager - invoke function
+
+    IO.puts "Passing fun3 as argument for fun1()"
+    IO.puts inspect fun1 fun3 # lazy evaluation and partial application, since we pass function as a value (without invoking the function!)
+
+    # more lazy evaluation:
+    result = [1,2,3,4,5,6,7,8,9]
+      |> Stream.drop(2)
+      |> Stream.take(5)
+      |> Stream.filter(fn element -> rem(element, 2) == 1 end) # let's assume it's some heavy processing
+      |> Stream.map(fn element -> element * 2 end) # + even more heavy processing
+    IO.puts inspect result # lazy, hence it's #Stream<…> - no invocation at all, so no processing at all!
+
+    other_required_state = true # simulate situation when something doesn't have to be processed at all if other conditions aren't satisfied - this way you can simply discard whole lazy processing.. and gain significant performance boost of your software
+    if other_required_state do
+      eager_result = result |> Enum.to_list() # Enum.to_list() evaluates eagerly, then
+      IO.puts inspect eager_result # print expected values: [6, 10, 14]
+    end
+  end
+
+end
+
+
+Main.main()
