bfdcd360d7a666f1f5d71f890c6752c9af1a4a3a
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-08-24 15:43:38 +0200

    new: Defined p3. Monads in action :)

---
+++ src/exs/p3.exs
@@ -0,0 +1,121 @@
+# Taken from Yst project written by me last year
+
+# Elixir lacks monadic processing, but we can implement our own:
+
+
+defmodule None do
+  @moduledoc """
+  Option None
+  """
+  @type t :: None.t
+
+  defstruct [:v, :vtype]
+
+  def v do
+    %None{}
+  end
+end
+
+
+defmodule Some do
+  @moduledoc """
+  Option Some
+  """
+  require Logger
+
+  @type t :: Some.t
+
+  @enforce_keys [:v, :vtype]
+  defstruct [:v, :vtype]
+
+  # This example also shows pattern matching
+  @spec v :: (Some.t | None.t)
+  def v, do: None.v
+  def v(nil), do: None.v
+  def v(<<>>), do: None.v
+  def v([]), do: None.v
+  def v({}), do: None.v
+  def v(%{}), do: None.v
+  def v(%Some{}), do: None.v
+  def v input do
+    in_type = typeof input
+    %Some{
+      v: input,
+      vtype: in_type,
+    }
+  end
+
+
+  @spec unwrap(some :: Some.t) :: any
+  def unwrap some do
+    try do
+      %Some{v: value} = some
+      value
+    rescue
+      MatchError ->
+        some
+
+      _ ->
+        some
+    end
+  end
+
+
+  @spec unwrap_or_else(some :: Some.t, elseblock :: Some.t) :: any
+  def unwrap_or_else some, elseblock do
+    try do
+      %Some{v: value} = some
+      value
+    rescue
+      _ ->
+        case elseblock do
+          %Some{v: value, vtype: _} -> value
+          val -> val
+        end
+    end
+  end
+
+
+  # Trick to get type of value in dynamic language.. all dynamic languages have their own "dragons", this is how it's done for Elixir (Erlang)
+  types = ~w[boolean number function nil integer binary bitstring list map float atom tuple pid port reference]
+
+  for type <- types do
+
+    # This is a macro that defines functions for each type from "types" (let's not dig too deep into macros for now)
+    def typeof(x) when unquote(:"is_#{type}")(x), do: unquote(type)
+
+  end
+
+end
+
+
+defmodule Main do
+
+  def main do
+    # Kinda hacky and magic, and not much gain here to be honest - but it's possible:
+
+    value = Some.v Keyword.new [some: "more", time: "for"]
+    %Some{v: [some: "more", time: "for"], vtype: "list"} = value
+
+    value2 = Some.v 2345
+    %Some{v: 2345, vtype: "number"} = value2
+
+    value3 = Some.v "some binary" # it's "binary" since Erlang has no real String value - 'values' is list of chars.
+    %Some{v: "some binary", vtype: "binary"} = value3
+
+    %None{} = Some.unwrap(Some.v())
+    12_345 = Some.unwrap(Some.v(12_345))
+
+    value_wrong = [even: "now"]
+    1 = value_wrong |> Some.unwrap_or_else(1)
+    3 = {:anything, :else} |> Some.unwrap_or_else(3)
+    {:anything, :else} = Some.v({:anything, :else}) |> Some.unwrap_or_else(3)
+    [some: "more", time: "for"] = value |> Some.unwrap()
+    [some: "more", time: "for"] = value |> (Some.unwrap_or_else :NOWAI)
+
+    IO.puts "All matches passed as expected"
+  end
+
+end
+
+Main.main()
---
+++ src/rs/p3b.rs
@@ -0,0 +1,61 @@
+// Rust is modern FP language hence it already has all basic monadic types built in:
+
+// Result monad in action:
+
+
+fn first_part(process_value: u64) -> Result<u64, String> {
+    Ok(process_value + 1)
+}
+
+
+fn second_part(process_value: u64) -> Result<u64, String> {
+    match process_value {
+        3 => Ok(process_value + 13),
+        value if value % 2 == 0 => Ok(value + 5),
+        _ => Err("Nie no nie bardzo".to_string()),
+    }
+}
+
+
+
+fn main() {
+    {
+        let result = first_part(1)
+                        .and_then(second_part)
+                        .and_then(first_part)
+                        .and_then(first_part)
+                        .unwrap_or(666);
+        println!("Value result 1: {}", result);
+    }
+    {
+        let result = first_part(2)
+                        .and_then(second_part)
+                        .unwrap_or(666);
+        println!("Value result 2: {}", result);
+    }
+    {
+        let result = first_part(3)
+                        .and_then(second_part)
+                        .unwrap_or(666);
+        println!("Value result 3: {}", result);
+    }
+    {
+        let result = first_part(11)
+                        .and_then(second_part)
+                        .unwrap_or(666);
+        println!("Value result 4: {}", result);
+    }
+    {
+        let result = first_part(10)
+                        .and_then(second_part) // this one will return Err(), so
+                        .and_then(first_part) // this is NO-OP here!!
+                        .and_then(second_part) // this is NO-OP here too!!
+                        .and_then(first_part) // you can have any amount of steps in a flow, but flow ends when Err happens so
+                        .map_err(|error| println!("Something went wrong here! Cause: {}!", error)) // we map our errors and print it out. We could do panic!() here to immediately terminate the application, but println macro is used for example purposes
+                        .unwrap_or(666); // since error was returned.. we return some erronous value
+        println!("Value result 5: {}", result);
+
+        // NOTE: now count how many if / else statements would you write here in imperative programming language :)
+    }
+}
+
