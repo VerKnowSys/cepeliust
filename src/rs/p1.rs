71644fa5745ccb3c9c9567db0ac9bdf53619605b
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-08-25 04:52:17 +0200

    new: Defined two additional examples in p1.rs

--- src/rs/p1.rs
+++ src/rs/p1.rs
@@ -19,6 +19,22 @@ mod operations {
         }
     }
 
+
+    /* sum list of values v1 */
+    pub fn sum_list1(vector: &Vec<usize>) -> usize {
+        vector
+            .iter()
+            .fold(0, |value, accum| value + *accum)
+    }
+
+
+    /* sum list of values v2 */
+    pub fn sum_list2(vector: &Vec<usize>) -> usize {
+        vector
+            .iter()
+            .sum()
+    }
+
 }
 
 
@@ -30,4 +46,8 @@ fn main() {
 
     /* 1st call */ println!("Rust: {}! = {}, sumall = {}", of, factorial(of), sumall(of));
     /* Nth call */ println!("Rust: 0! = {}, sumall = {}", factorial(0), sumall(0));
+
+    let a_list = vec!(1,2,3,4,5,6,7,8,9,10);
+    println!("a_list1: {:?}", &a_list);
+    println!("Sum_list1: {}, Sum_list2: {}", sum_list1(&a_list), sum_list2(&a_list));
 }
