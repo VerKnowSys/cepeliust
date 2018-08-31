6b1cbaaed061a4af3baf75e214da6fe091e0362e
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-08-31 15:27:04 +0200

    imp: Split long lines in p1.rs

--- src/rs/p1.rs
+++ src/rs/p1.rs
@@ -44,10 +44,15 @@ fn main() {
     // let of = 0;
     let of = 10;
 
-    /* 1st call */ println!("Rust: {}! = {}, sumall = {}", of, factorial(of), sumall(of));
-    /* Nth call */ println!("Rust: 0! = {}, sumall = {}", factorial(0), sumall(0));
+    /* 1st call */
+    println!("Rust: {}! = {}, sumall = {}",
+             of, factorial(of), sumall(of));
+    /* Nth call */
+    println!("Rust: 0! = {}, sumall = {}",
+             factorial(0), sumall(0));
 
     let a_list = vec!(1,2,3,4,5,6,7,8,9,10);
     println!("a_list1: {:?}", &a_list);
-    println!("Sum_list1: {}, Sum_list2: {}", sum_list1(&a_list), sum_list2(&a_list));
+    println!("Sum_list1: {}, Sum_list2: {}",
+             sum_list1(&a_list), sum_list2(&a_list));
 }
